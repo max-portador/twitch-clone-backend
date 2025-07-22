21a1197573a54cbe53cba61ec50bb7f6c09043af
max-portador <deus7@yandex.ru>
2025-07-22 14:14:50 +0000

    feat: add session and cors in bootstrap method, add redis in docker-compose

---
+++ src/core/config/graphql.config.ts
@@ -0,0 +1,16 @@
+import { ApolloDriverConfig } from './../../../node_modules/@nestjs/apollo/dist/interfaces/apollo-driver-config.interface.d'
+import { ConfigService } from '@nestjs/config'
+import { isDev } from 'src/shared/utils/isDev.util'
+import { join } from 'path'
+
+export function getGraphQLConfig(
+	configService: ConfigService
+): ApolloDriverConfig {
+	return {
+		playground: isDev(configService),
+		path: configService.getOrThrow<string>('GRAPHQL_PREFIX'),
+		autoSchemaFile: join(process.cwd(), 'src/core/graphql/schema.gql'),
+		sortSchema: true,
+		context: ({ req, res }) => ({ req, res })
+	}
+}
--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,8 +1,28 @@
+import { ConfigModule, ConfigService } from '@nestjs/config'
+
+import { ApolloDriver } from '@nestjs/apollo'
+import { GraphQLModule } from '@nestjs/graphql'
+import { IS_DEV_ENV } from 'src/shared/utils/isDev.util'
 import { Module } from '@nestjs/common'
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from './prisma/prisma.module'
+import { getGraphQLConfig } from './config/graphql.config'
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-	imports: [PrismaModule],
+	imports: [
+		ConfigModule.forRoot({
+			ignoreEnvFile: !IS_DEV_ENV,
+			isGlobal: true
+		}),
+		GraphQLModule.forRootAsync({
+			driver: ApolloDriver,
+			imports: [ConfigModule],
+			useFactory: getGraphQLConfig,
+			inject: [ConfigService]
+		}),
+		PrismaModule,
+		RedisModule
+	],
 	controllers: [],
 	providers: []
 })
