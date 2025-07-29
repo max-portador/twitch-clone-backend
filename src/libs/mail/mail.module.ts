4ea6eaf679886d90435ceca55c8b67c1f0521298
portador <deus7@protonmail.com>
2025-07-30 00:03:20 +0300

    feat: add mail module

---
+++ src/libs/mail/mail.module.ts
@@ -0,0 +1,19 @@
+import { getMailerConfig } from '@/src/core/config/mailer.config';
+
+import { MailService } from './mail.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+	imports: [
+		MailerModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: getMailerConfig,
+			inject: [ConfigService],
+		}),
+	],
+	providers: [MailService],
+	exports: [MailService],
+})
+export class MailModule {}
